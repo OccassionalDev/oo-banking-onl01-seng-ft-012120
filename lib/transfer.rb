5efe3fc189d25a7e6f7346bbabf628d32efdccff
Erica Schimmel <ericaschimmel01@gmail.com>
2020-02-06 03:19:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,14 +1,13 @@
 class Transfer
   # your code here
   attr_accessor :status
-  attr_reader :amount, :sender, :receiver, :transfered_once
+  attr_reader :amount, :sender, :receiver
   
   def initialize(sender, receiver, amount)
     @sender = sender 
     @receiver = receiver
     @amount = amount 
     @status = "pending"
-    @transfered_once = false
   end
   
   def valid?
@@ -21,14 +20,13 @@ class Transfer
   end
   
   def execute_transaction 
-    if @transfered_once != false && self.sender.balance < amount 
+    if @status != "pending" || self.sender.balance < amount 
       return nil 
       
     else 
       self.sender.balance -= amount 
       self.receiver.balance += amount 
       @status = "complete"
-      @transfered_once = true
     end 
   end 
 end

b36704a17368b062fed1ad8854b637679e349fa1
Erica Schimmel <ericaschimmel01@gmail.com>
2020-02-06 03:08:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,4 +1,12 @@
 class Transfer
   # your code here
   attr_accessor :status
+  attr_reader :amount, :sender, :receiver
+  
+  def intialize(sender, receiver, amount)
+    @sender = sender 
+    @receiver = receiver
+    @amount = amount 
+    @status = "pending"
+  end 
 end
